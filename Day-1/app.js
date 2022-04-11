e2915db0769325750852caed4f92a791642b2a2c
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-11 10:16:14 +0530

    jQuery syntax | Firs program

--- Day-1/app.js
+++ Day-1/app.js
@@ -1,1 +1,16 @@
-console.log($);
+// console.log($);          Check the working of jQuery
+
+// General syntax of jQuery
+
+/*
+    $('<selector'>).action()
+        Eg: $('p').hide()
+*/
+
+// First Program
+
+$('h1').click(function () {
+    console.log("You clicked on <h1>");
+
+    // $('h1').hide()
+})
