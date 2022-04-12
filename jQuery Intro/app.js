83bc65bbb0fdd41ac9850c429afdd824c0ec9d93
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-12 10:15:15 +0530

    this in jQuery

--- jQuery Intro/app.js
+++ jQuery Intro/app.js
@@ -9,8 +9,9 @@
 
 // First Program ==> do this when h1 tag is clicked
 
-$('h1').click(() =>{
+$('h1').click(function (){      // don't use arrow function
     console.log("You clicked on <h1>");
 
-    // $('h1').hide()
+    // $('h1').hide()      // hides all the h1 tags
+    $(this).hide()          // Hides the specific object that is clicked upon
 })
