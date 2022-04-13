7afa733b81b35eb7610a765c2be9bf00d14cde21
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-13 11:05:58 +0530

    hide function | html text added

--- Day-2/app.js
+++ Day-2/app.js
@@ -1,3 +1,13 @@
 /*
-    Event Methods
+    Event Methods == keyboard events
 */
+
+$(document).ready(function(){
+    // hide and show events
+
+    // .hide function takes time as milliseconds and takes that time to hide the object.
+
+    $('#learning').hide(3000, function(){
+        console.log("Content Hidden");
+    })
+})
