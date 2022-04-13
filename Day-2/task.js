3f3ecc754bd67e2adac9719f1de8a921c267df94
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-13 12:02:31 +0530

    form and keyboard events

--- Day-2/task.js
+++ Day-2/task.js
@@ -1,3 +1,33 @@
 $(document).ready(function(){
-    
+
+    // Fires when the key is being pressed
+    $(document).keydown(function(){
+        console.log("Key is being pressed");
+    })
+
+    // Fires when the key is stopped pressing
+    $(document).keyup(function(){
+        console.log("Key is up");
+    })
+
+    // Fires only when the key is pressed
+    $(document).keypress(function(){
+        console.log("key pressed");
+    })
+
+    // FORM EVENTS
+
+    $(document).on('submit', '#taskForm', function() {
+        return false;
+       });
+
+    $("#taskForm").submit(function(){
+        console.log("Form submitted");
+    })
+    $("#taskInput").blur(function(){
+        console.log("Focus changed from form");
+    })
+    $("#taskInput").focus(function(){
+        console.log("Focus changed to form");
+    })
 })
