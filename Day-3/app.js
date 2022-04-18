e633133f2ae38b83fcf4da562387144ff5762762
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-18 10:02:20 +0530

    What is AJAX?

---
+++ Day-3/app.js
@@ -0,0 +1,5 @@
+/*
+    What is AJAX?
+
+    Sol. AJAX stand for Asynchronous JavaScript and XML. It is a new technique for creating better, faster, and more interactive web applications with help of XML, HTML, CSS and JavaScript.
+*/
