833743da0f035dacceca76f49fe0ab9b3beaf162
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-18 10:05:38 +0530

    adding element methods

--- Day-3/app.js
+++ Day-3/app.js
@@ -3,3 +3,11 @@
 
     Sol. AJAX stand for Asynchronous JavaScript and XML. It is a new technique for creating better, faster, and more interactive web applications with help of XML, HTML, CSS and JavaScript.
 */
+
+/*
+    Add new elements:
+        1. append() ==> inserts content at the end of the selected element
+        2. prepend() ==> inserts content at the beginning of the seleted element
+        3. after() ==> inserts content after the selected elements
+        4. before() ==> inserts content before the selected elements
+*/
