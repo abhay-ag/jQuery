9fd6ff1221c91db2a16aa9a55dd7ba1c224b758a
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-19 10:17:39 +0530

    more about AJAX

--- Day-4/app.js
+++ Day-4/app.js
@@ -1,6 +1,11 @@
-$(document).ready(() => {
-
-})
 // AJAX is not a language. 
 // data recieved from server in form on JSON ==> JavaScript Objeect Notation
 // Some sites send in the form of XML
+
+// XML is used for recieving data from serverm although any format including plian text can be used
+
+/* 
+    Ajax uses XHTML for content, CSS for presentation along with document object model and JavaScript for dynamic content display.
+
+    It is a web browser technology independent of web server software.
+*/
