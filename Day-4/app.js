81ae47566d18a088b599c90a439b7c4ba7de4fad
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-19 10:23:40 +0530

    API | Usage of AJAX

--- Day-4/app.js
+++ Day-4/app.js
@@ -2,10 +2,13 @@
 // data recieved from server in form on JSON ==> JavaScript Objeect Notation
 // Some sites send in the form of XML
 
-// XML is used for recieving data from serverm although any format including plian text can be used
+// XML is used for recieving data from server although any format including plian text can be used
 
 /* 
     Ajax uses XHTML for content, CSS for presentation along with document object model and JavaScript for dynamic content display.
 
     It is a web browser technology independent of web server software.
 */
+
+// Where there is a need for real time data AJAX is used.
+// API ==> Application Programming Interface
