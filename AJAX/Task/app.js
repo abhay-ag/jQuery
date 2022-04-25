be92a54ec86b82d6d428f1ffb1503cb7ef20af8c
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-25 10:11:27 +0530

    JSON created | Data fetched conditionally

---
+++ AJAX/Task/app.js
@@ -0,0 +1,43 @@
+const getBtn = document.querySelector('.get')
+const sendBtn = document.querySelector('.send')
+const fetchBtn = document.querySelector('.fetch')
+const grt35 = document.querySelector('.grt35')
+const grt10000 = document.querySelector('.grt10000')
+let obj = {}
+
+getBtn.addEventListener('click', () => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'package.json', true)
+
+    xhr.onload = function() {
+        obj = JSON.parse(this.responseText);
+    }
+    
+    xhr.send()
+})
+sendBtn.addEventListener('click', () =>{
+    let str = ''
+
+    let data = obj.data
+    for (let i = 0; i < data.length; i++){
+        if(data[i].age > 35){
+            str += `<li>${data[i].name}</li>`
+        }
+    }
+
+    grt35.innerHTML = str
+})
+
+fetchBtn.addEventListener('click', () => {
+    console.log(obj.data[0].salary);
+    let str = ''
+
+    let data = obj.data
+    for( let i =0; i < data.length; i++){
+        if(data[i].salaray > 10000){
+            str += `<li>${data[i].name}, ${data[i].age}</li>`
+        }
+    }
+    grt10000.innerHTML = str
+})
