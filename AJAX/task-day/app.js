2fa2f06e45515dac6f9c4f3edce5831f95cc8fde
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-27 13:25:01 +0530

    bugs fixed

--- AJAX/task-day/app.js
+++ AJAX/task-day/app.js
@@ -21,30 +21,39 @@ subBtn.addEventListener('click', () =>{
     const uEnter = un.value
     const pEnter = ps.value
     let flag = false
-
-    un.value = ''
-    ps.value = ''
-
-    for(let i = 0; i < obj.data.length; i++){
-        if(uEnter === obj.data[i].uName && pEnter === obj.data[i].pswrd){
-            flag = true;
-            break;
-        }
+    if(uEnter === '' || pEnter === ''){
+        un.style.cssText = 'border-bottom: 1px solid red'
+        ps.style.cssText = 'border-bottom: 1px solid red'
+        setTimeout(() => {
+            un.style.cssText = 'border-bottom: 1px solid #8d8d8d'  
+            ps.style.cssText = 'border-bottom: 1px solid #8d8d8d'  
+        }, 1000);
     }
-
-    if(flag){
-        green.classList.add('show')
-        red.classList.remove('show')
-        setTimeout(() => {
-            green.classList.remove('show')
+    else{
+        un.value = ''
+        ps.value = ''
+    
+        for(let i = 0; i < obj.data.length; i++){
+            if(uEnter === obj.data[i].uName && pEnter === obj.data[i].pswrd){
+                flag = true;
+                break;
+            }
+        }
+    
+        if(flag){
+            green.classList.add('show')
             red.classList.remove('show')
-        }, 1500);
-    }else{
-        red.classList.add('show')
-        green.classList.remove('show')
-        setTimeout(() => {
-            red.classList.remove('show')
+            setTimeout(() => {
+                green.classList.remove('show')
+                red.classList.remove('show')
+            }, 1500);
+        }else{
+            red.classList.add('show')
             green.classList.remove('show')
-        }, 1500);
+            setTimeout(() => {
+                red.classList.remove('show')
+                green.classList.remove('show')
+            }, 1500);
+        }
     }
 })
